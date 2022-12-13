2a88e8b29c4e4c6fc43e27fc154e8ee867659892
Emma Rünzel <emma@ruenzel.de>
2022-12-13 15:56:04 +0100

    Adding a Navbar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/dashboard_controller.rb
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-  def admin; end
+  def admin
+    @should_render_navbar = true
+  end
 end
