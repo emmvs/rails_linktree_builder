53342ea1abd5e01e17ba53f0301624826d04bb09
Emma Rünzel <emma@ruenzel.de>
2022-12-16 12:54:43 +0100

    User Show page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,12 +6,13 @@ class DashboardController < ApplicationController
   end
 
   def show
-
+    redirect_to dashboard_path if @user.nil?
   end
 
   private
 
   def set_user
-
+    # localhost:3000
+    @user = User.find_by_id(params[:id])
   end
 end
