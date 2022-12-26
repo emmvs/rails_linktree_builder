2812b9edf2edc4e8f3ea2c974ef3cf29770ae8c3
Emma Rünzel <emma@ruenzel.de>
2022-12-26 20:36:27 +0100

    lt fb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/dashboard_controller.rb
 class DashboardController < ApplicationController
-  before_action :authenticate_user!, except: 
+  before_action :authenticate_user!, except:
   before_action :set_user, only: [:show]
 
   def admin
@@ -14,7 +14,7 @@ class DashboardController < ApplicationController
   private
 
   def set_user
-    # Using find_by_id instead of find in case of text input
+    # Using find_by_id instead of // find in case of text input
     @user = User.find_by_id(params[:id])
   end
 end
