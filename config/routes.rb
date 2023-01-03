db613f09640622bcb0bf944790f3bf0691c3ba65
Emma Rünzel <emma@ruenzel.de>
2023-01-03 14:20:33 +0100

    added sessions & registrations to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get 'dashboard', to: 'dashboard#admin'
   root 'dashboard#admin'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Gets the id as a parameter to our dashboard_controller within the admin action
   # Allows us to use user_path(user) to get the URL for a user's profile page
   get ':id', to: 'dashboard#show', as: :user
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
