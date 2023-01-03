642ce19f974d16ccbc1f7dcdef9c8be48d538b17
Emma Rünzel <emma@ruenzel.de>
2023-01-03 14:25:07 +0100

    Added appearance action (which renders the navbar, too) to dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,10 @@ class DashboardController < ApplicationController
     @should_render_navbar = true
   end
 
+  def appearance
+    @should_render_navbar = true
+  end
+
   def show
     redirect_to dashboard_path if @user.nil?
   end
