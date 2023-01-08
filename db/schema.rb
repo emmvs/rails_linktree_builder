148b84ae5c9e3ac6a326a020712dd07b7d9ec78b
Emma Rünzel <emma@ruenzel.de>
2023-01-08 19:25:18 +0100

    Added useranme, full_name & body to User II

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_131228) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_182327) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,8 +47,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_131228) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "full_name"
+    t.text "body"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
