b078ad8c8669566fd1de73baef75ffd434b1cf6d
Emma Rünzel <emma@ruenzel.de>
2023-01-09 09:30:16 +0100

    Generated links_controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,16 @@
+# app/controllers/links_controller.rb
+class LinksController < ApplicationController
+  before_action :authenticate_user!, only: [:update]
+  before_action :link_params, only: [:update]
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update(link_params)
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
+end
