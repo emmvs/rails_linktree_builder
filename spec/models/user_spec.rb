290e5a153fed59dcc6166bef5c78f8f96c2673a6
Emma A. A. Rünzel <emma@ruenzel.de>
2023-05-29 19:56:02 +0200

    Add first User Model Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
 
-# This is a comment to show branch adding 
+# This is a comment to show branch adding
+
+describe User do
+  it "should have a name" do
+    expect(User.new(name: "Drew")).to be_valid
+  end
+  it "should have an email" do
+    expect(User.new(email: "drew@test.com")).to be_valid
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,3 @@
-# spec/rails_helper.rb
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -62,9 +60,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # Added this for testing porposes
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
 end
