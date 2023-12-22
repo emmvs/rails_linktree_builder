87205b65e8aa6ebca26a845e4693819be63d6a4f
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-22 22:26:54 +0100

    Create Migration f/ Clicks Table

---
+++ db/migrate/20231222200627_create_clicks.rb
@@ -0,0 +1,15 @@
+class CreateClicks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clicks do |t|
+      t.references :link, null: false, foreign_key: true
+      t.string :user_agent
+      t.string :device_type
+      t.string :country
+      t.string :city
+      t.datetime :clicked_at
+
+      t.timestamps
+    end
+    add_index :clicks, :country
+  end
+end
