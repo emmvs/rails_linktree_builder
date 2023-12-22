febcea55462d60509f24832350ffe50bccfb2c9e
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-22 22:21:32 +0100

    Refactor Track Click Method w/ Click Record Creation

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -22,19 +22,21 @@ class LinksController < ApplicationController
 
   def track_click
     @link.increment!(:click_count)
-    @link.update!(last_clicked_at: Time.current)
-    @link.user_agent = request.user_agent
-    browser = Browser.new(request.user_agent)
-    @link.device_type = browser.device.name
+    # @link.update!(last_clicked_at: Time.current)
+    @link.last_clicked_at = Time.current
 
-    results = Geocoder.search(request.remote_ip)
-    if results.present?
-      @link.country = results.first.country
-      @link.city = results.first.city
+    begin
+      create_click_record(request.remote_ip)
+    rescue => e
+      Rails.logger.error "Error in track_click: #{e.message}"
+      redirect_to error_path, alert: "Ooops! An error occurred. 😱" and return
     end
 
-    @link.save
-    redirect_to @link.url, allow_other_host: true
+    if @link.save
+      redirect_to @link.url, allow_other_host: true
+    else
+      redirect_to error_path, alert: "Error processing your request."
+    end
   end
 
   private
@@ -45,5 +47,48 @@ class LinksController < ApplicationController
 
   def set_link
     @link = Link.find(params[:id])
+  end
+
+  def create_click_record(remote_ip)
+    @link.clicks.create(
+      user_agent: request.user_agent,
+      device_type: determine_device_type(request.user_agent),
+      country: determine_country(remote_ip),
+      city: determine_city(remote_ip),
+      clicked_at: Time.current
+    )
+  end
+
+  def determine_country(remote_ip)
+    results = Geocoder.search(remote_ip)
+    if results.present? && !results.first.data["bogon"]
+      results.first.country
+    else
+      "Local"
+    end
+  end
+
+  def determine_city(remote_ip)
+    results = Geocoder.search(remote_ip)
+    if results.present? && !results.first.data["bogon"]
+      results.first.city
+    else
+      "Local"
+    end
+  end
+
+  def determine_device_type(user_agent)
+    browser = Browser.new(user_agent)
+    if browser.device.mobile?
+      "Mobile"
+    elsif browser.device.tablet?
+      "Tablet"
+    elsif browser.device.tv?
+      "TV"
+    elsif browser.known?
+      "Desktop"
+    else
+      "Unknown"
+    end
   end
 end
