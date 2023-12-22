4ae2c2bdaf5630974e746b6b3e1134116d2db3e3
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-22 20:33:33 +0100

    Enhance LinksController w/ Error Handling & Click Tracking

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,23 +1,49 @@
-# app/controllers/links_controller.rb
 class LinksController < ApplicationController
-  before_action :authenticate_user!, only: [:update]
+  before_action :authenticate_user!, only: [:create, :update]
+  before_action :set_link, only: [:update, :track_click]
 
   def create
     @link = Link.new(link_params)
     @link.user = current_user
-    @link.save
-    redirect_to dashboard_path, notice: "Link was successfully created. 🎉"
+    if @link.save
+      redirect_to dashboard_path, notice: "Link was successfully created. 🎉"
+    else
+      render :new, alert: "Error creating link. 😱"
+    end
   end
 
   def update
-    @link = Link.find(params[:id])
-    @link.update(link_params)
-    redirect_to dashboard_path, notice: "Link was successfully updated. 🎉"
+    if @link.update(link_params)
+      redirect_to dashboard_path, notice: "Link was successfully updated. 🎉"
+    else
+      render :edit, alert: "Error updating link. 😱"
+    end
+  end
+
+  def track_click
+    @link.increment!(:click_count)
+    @link.update!(last_clicked_at: Time.current)
+    @link.user_agent = request.user_agent
+    browser = Browser.new(request.user_agent)
+    @link.device_type = browser.device.name
+
+    results = Geocoder.search(request.remote_ip)
+    if results.present?
+      @link.country = results.first.country
+      @link.city = results.first.city
+    end
+
+    @link.save
+    redirect_to @link.url, allow_other_host: true
   end
 
   private
 
   def link_params
     params.require(:link).permit(:title, :url)
+  end
+
+  def set_link
+    @link = Link.find(params[:id])
   end
 end
