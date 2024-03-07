48be2d31159153cb22ff9446bd893c5ffb296980
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-07 21:19:25 +0100

    Update routes.rb w/o Unnecessary Routes & w/ User Redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,38 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   get 'pages/about'
-  get 'pages/contact'
   get 'static/error'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  constraints ->(request) { request.env['warden'].user } do
+    #              ☝🏻 current HTTP request object
+    #                  Returns Raw Reck Env: Rack is a low-level interface between web servers that support Ruby and Ruby frameworks.
+    #                          ☝🏻 Essentially, every Rails application is also a Rack application
+    #                         .user fetches the currently authenticated user
+    root 'dashboards#index', as: :authenticated_root
+  end
+
+  root 'pages#about'
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-
-  get 'dashboard', to: 'dashboard#index'
-  get 'appearance', to: 'dashboard#appearance'
-
-  root 'dashboard#index'
-
+  
+  get 'dashboards', to: 'dashboards#index'
+  get 'appearance', to: 'dashboards#appearance'
+  
+  
   get 'links/:id/track_click', to: 'links#track_click', as: :track_link_click
-
+  
   get '/error', to: 'static#error', as: :error
 
-  # Gets the id as a parameter to our dashboard_controller within the index action
+  # QR Codes that lead to App Stores
+  get '/app_stores', to: 'pages#app_stores', as: :app_stores
+
+  # Gets the id as a parameter to our dashboards_controller within the index action
   # Allows us to use user_path(user) to get the URL for a user's profile page
-  get ':id', to: 'dashboard#show', as: :user
+  get ':id', to: 'dashboards#show', as: :user
 
   resources :links, only: %i[create update]
 
